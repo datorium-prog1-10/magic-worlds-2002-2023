5955fd2ee05719186f3bdcbd031672a21350245e
& C:/Users/Elchin/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Elchin/OneDrive/Documents/Codes/Python/json-thu1200/script.py <hello@datorium.eu>
2023-04-05 14:18:01 +0300

    Game balance adjusted

--- prog1-10-1200/part2/wordls.py
+++ prog1-10-1200/part2/wordls.py
@@ -1,3 +1,5 @@
+import random
+
 class Wizard:
     count = 0
     list = []
@@ -59,21 +61,37 @@ class Dragon:
 
         print(f"💥 {self.name} attacks {target.name}!")
         self.energy -= 100
-        target.health -= 30
+        target.health -= 50
+
+humans = []
+humans_removed = []
+dragons = []
 
 def main():
-    humans = []
-    dragons = []
-
     for i in range(100):
         humans.append(Human(f"Name{i}"))
 
     for i in range(10): 
-        dragons.append(Dragon(f"Dragon{i}", 200))
+        dragons.append(Dragon(f"Dragon{i}", 1000))
 
     # UZDEVUMS:
     # Izveido programmu kas 20 reizes nejauši
     # izvēlēsies vienu pūķi un vienu cilvēku
     # un attiecīgi tas pūķis uzbruks cilvēkam
 
+    for i in range(200):
+        random_dragon = random.choice(dragons)
+        random_human = random.choice(humans)
+        random_dragon.attack(random_human)
+
+    # UZDEVUMS:
+    # Ja kādam cilvēkam beidzās veselība tad
+    # lai izņem to no saraksta `humans` un 
+    # ieliec to citā sarakstā `humans_removed`
+
+        if random_human.health <= 0:
+            humans.remove(random_human)
+            humans_removed.append(random_human)
+
+    print(len(humans_removed))
 main()
